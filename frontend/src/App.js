d8dc9b24e9fbc8088bc8fa736b8c200d83834273
Nikola Bozovic <nikola00bozovic@gmail.com>
2025-01-03 18:10:23 +0100

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header'
 import Search from './components/Search';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
 function App() {
   const [word, setWord] = useState('');
 
   const handleSearchSubmit = (e) => {
     e.preventDefault(); // to not reload on submit
     console.log(word);
+
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   }
-  
+
   return (
     <div className="App">
       <Header title="Images Galery"/>
