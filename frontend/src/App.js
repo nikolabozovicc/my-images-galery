1fe6e60d336ddf5004c4fcd86d85ff3d7bfe4bac
Nikola Bozovic <nikola00bozovic@gmail.com>
2025-01-09 17:24:14 +0100

    changed api in the frontend and enabled cors in api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import ImageCard from './components/ImageCard';
 import { Container, Row, Col } from 'react-bootstrap';
 import Welcome from './components/Welcome';
 
-const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5050';
 
 function App() {
   const [word, setWord] = useState('');
@@ -15,9 +15,7 @@ function App() {
   const handleSearchSubmit = (e) => {
     e.preventDefault(); // to not reload on submit
 
-    fetch(
-      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
-    )
+    fetch(`${API_URL}/new-image?query=${word}`)
       .then((res) => {
         if (!res.ok) {
           // Handle HTTP errors by returning the response text or JSON to the next then
