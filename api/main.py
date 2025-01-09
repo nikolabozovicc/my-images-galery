de4d3e0d244b12f3ce87ec30e9d7fc5d09f339e6
Nikola Bozovic <nikola00bozovic@gmail.com>
2025-01-09 14:48:46 +0100

    enable DEBUG mode in flask application(to restart server on save)

--- api/main.py
+++ api/main.py
@@ -5,13 +5,16 @@ from dotenv import load_dotenv
 
 load_dotenv(dotenv_path="./.env.local")
 
-app = Flask(__name__)
-
 UNSPLASH_URL = "https://api.unsplash.com/photos/random"
 UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+DEBUG = os.environ.get("DEBUG", True)
 
 if not UNSPLASH_KEY:
     raise EnvironmentError("Please create .env.local file and insert there UNSPLASH_KEY")
+
+app = Flask(__name__)
+
+app.config["DEBUG"] = DEBUG
 
 @app.route("/new-image")
 def new_image():
